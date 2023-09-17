83d25c0915528536719071107d5737ba7cc261d6
rovin99 <rovin.singh.it@ghrce.raisoni.net>
2023-09-17 23:42:27 +0530

    added seeders

--- src/routes/v1/airplane-routes.js
+++ src/routes/v1/airplane-routes.js
@@ -1,12 +1,14 @@
 const express = require('express');
 
 const { AirplaneController } = require('../../controllers');
-
+const {AirplaneMiddleware } = require('../../middleware');
 
 const router = express.Router();
 
 // /api/v1/airplanes POST
-router.post('/', 
+router.post('/', AirplaneMiddleware.validateCreateAirplane,
         AirplaneController.createAirplane);
+router.get('/',AirplaneController.getAirplanes);
+//router.get('/:id',AirplaneController.getAirplane);
 
 module.exports = router;
